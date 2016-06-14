9de2a7e389caf9e522b0a7dd892aa07864d86e36
熊焕鑫 <xionghx@sc.10086.cn.defaultbadlist>
2016-06-14 15:02:02 +0800

    Initial Commit

---
+++ account book/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  account book
+//
+//  Created by xionghuanxin on 6/14/16.
+//  Copyright © 2016 xionghuanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ account book/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  account book
+//
+//  Created by xionghuanxin on 6/14/16.
+//  Copyright © 2016 xionghuanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
